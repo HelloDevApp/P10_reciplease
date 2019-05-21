4b3a25d1dd917f7b58f76bec88fe4a63daac44fe
Dylan <nais95100@icloud.com>
2019-05-21 21:27:33 +0200

    project creation

---
+++ P10_Reciplease/P10_Reciplease/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P10_Reciplease
+//
+//  Created by macbook pro on 21/05/2019.
+//  Copyright © 2019 macbook pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ P10_Reciplease/P10_RecipleaseTests/P10_RecipleaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  P10_RecipleaseTests.swift
+//  P10_RecipleaseTests
+//
+//  Created by macbook pro on 21/05/2019.
+//  Copyright © 2019 macbook pro. All rights reserved.
+//
+
+import XCTest
+@testable import P10_Reciplease
+
+class P10_RecipleaseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
