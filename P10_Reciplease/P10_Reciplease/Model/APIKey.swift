81da4790ae397dac686ed9c61217b7e05cabb8c5
Dylan <nais95100@icloud.com>
2019-06-11 17:54:06 +0200

    fix missing files

---
+++ P10_Reciplease/P10_Reciplease/Model/APIKey.swift
@@ -0,0 +1,24 @@
+//
+//  APIKey.swift
+//  P10_Reciplease
+//
+//  Created by macbook pro on 11/06/2019.
+//  Copyright © 2019 macbook pro. All rights reserved.
+//
+
+import Foundation
+
+struct APIKey {
+    
+    private let _key = ""
+    private let _id = ""
+    
+    var key: String {
+        return _key
+    }
+    
+    var id: String {
+        return _id
+    }
+    
+}
