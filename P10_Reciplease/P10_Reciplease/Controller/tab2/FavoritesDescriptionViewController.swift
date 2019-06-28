914bfeaa3356050829ec450997965205481c6464
Dylan <nais95100@icloud.com>
2019-06-28 12:26:20 +0200

    - Add a variable that will receive the selected recipe

--- P10_Reciplease/P10_Reciplease/Controller/tab2/FavoritesDescriptionViewController.swift
+++ P10_Reciplease/P10_Reciplease/Controller/tab2/FavoritesDescriptionViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class FavoritesDescriptionViewController: UIViewController {
     
+    var recipe: Recipe_?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
