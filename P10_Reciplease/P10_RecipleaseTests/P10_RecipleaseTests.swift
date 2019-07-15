bab91303a5e416ad5524e50a60a3d046884801ac
Dylan <nais95100@icloud.com>
2019-07-15 22:43:05 +0200

    - Reorganization of the file hierarchy for better navigation in the project

--- P10_Reciplease/P10_RecipleaseTests/P10_RecipleaseTests.swift
+++ P10_Reciplease/P10_RecipleaseTests/P10_RecipleaseTests.swift
@@ -11,68 +11,7 @@ import XCTest
 
 class P10_RecipleaseTests: XCTestCase {
     
-    let fakeResponseData = FakeResponseData()
-    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
-    func createFakeResponseAndFakeSession(data: Data?, response: HTTPURLResponse?, error: Error?) -> FakeAPISession {
-        let fakeResponse = FakeResponse(data: data, response: response, error: error)
-        let fakeSession = FakeAPISession(fakeResponse: fakeResponse)
-        return fakeSession
-    }
-    
-    func createAPIHelper(data: Data?, response: HTTPURLResponse?, error: Error?) -> APIHelper {
-        let fakeSession = createFakeResponseAndFakeSession(data: data, response: response, error: error)
-         let apiHelper = APIHelper(session: fakeSession)
-        return apiHelper
-    }
-    
-    // Alamofire
-    func testGetRecipesShouldPostFailedCallbackIfError() {
-        let apiHelper = createAPIHelper(data: nil, response: nil, error: fakeResponseData.error)
-        
-        apiHelper.getRecipe(userIngredients: []) { (apiResult, errorNetwork) in
-            XCTAssertNil(apiResult)
-            XCTAssertEqual(errorNetwork, ErrorNetwork.requestHasFailed)
-        }
-    }
-    
-    func testGetRecipesShouldPostFailedCallbackIfIncorrectData() {
-        let apiHelper = createAPIHelper(data: fakeResponseData.incorrectJSON, response: fakeResponseData.responseOK, error: nil)
-        
-        apiHelper.getRecipe(userIngredients: []) { (apiResult, errorNetwork) in
-            XCTAssertNil(apiResult)
-            XCTAssertEqual(errorNetwork, ErrorNetwork.wrongJSON)
-        }
-    }
-    
-    func testGetRecipesShouldPostSuccessCallbackIfNoErrorAndCorrectData() {
-        let apiHelper = createAPIHelper(data: fakeResponseData.jsonOK, response: fakeResponseData.responseOK, error: nil)
-        
-        apiHelper.getRecipe(userIngredients: []) { (apiResult, errorNetwork) in
-            XCTAssert(!apiResult!.hits.isEmpty)
-            XCTAssertEqual(errorNetwork, ErrorNetwork.noError)
-        }
-    }
-    
-    func testGetRecipesShouldPostSuccessCallbackWithEmptyRecipes() {
-        let apiHelper = createAPIHelper(data: fakeResponseData.jsonWithEmpyRecipes, response: fakeResponseData.responseOK, error: nil)
-        
-        apiHelper.getRecipe(userIngredients: []) { (apiResult, errorNetwork) in
-            XCTAssertNotNil(apiResult)
-            XCTAssertEqual(errorNetwork, ErrorNetwork.noRecipeFound)
-        }
-    }
-    
-    func testGetRecipesShouldPostFailedCallbackIfResponseIsNotCorrect() {
-        let apiHelper = createAPIHelper(data: fakeResponseData.jsonOK, response: fakeResponseData.responseNotOK, error: fakeResponseData.error)
-        
-        apiHelper.getRecipe(userIngredients: []) { (apiResult, errorNetwork) in
-            XCTAssertNil(apiResult)
-            XCTAssertEqual(errorNetwork, ErrorNetwork.requestHasFailed)
-        }
-    }
-
 }
